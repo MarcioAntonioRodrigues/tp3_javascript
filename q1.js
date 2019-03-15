ad9f992ca50ba43974c8e7142f602229d14dab6d
Marcio Antonio Rodrigues de Souza <marcio.dsouza@al.infnet.edu.br>
2019-03-14 21:51:16 -0300

    commitando novamente a questão 1

--- q1.js
+++ q1.js
@@ -5,6 +5,7 @@
 	let input1 = document.createElement("input");
 	let input2 = document.createElement("input");
 	let btn = document.createElement("button");
+	let paragraph = document.createElement("p");
 
 	label1.innerHTML = "Valor minimo";
 	label1.style.display = "block";
@@ -26,41 +27,36 @@
 	q1.appendChild(label2);
 	q1.appendChild(input2);
 	q1.appendChild(btn);
+	q1.appendChild(paragraph);
 
-	
-
-
-
-
-	var resp1 = document.getElementById('resp1');
-	let vmin = document.getElementById('vmin').value;
-	let vmax = document.getElementById('vmax').value;
-	if(vmin === '' || vmax === '')
+	btn.onclick = function()
 	{
-		window.alert("Preencha os campos!");
-	}
-	else
-	{
-		// vmin = vmin.value;
-		// vmax = vmax.value;
-		if(vmin < vmax)
+		let vmin = input1.value;
+		let vmax = input2.value;
+		if(vmin === '' || vmax === '')
 		{
-			var a = 0;
-			var list = [];
-			for(var i = vmin; i < vmax; i++)
-			{
-				if(i % 2 === 0 && i % 3 === 0 && i !== vmin && i !== vmax)
-				{
-					list.push(i);
-				}
-			}
-			a = list.length;
-			// return "Iniciando Algoritmo Nr1 - \n A resposta é: " + a;
+			window.alert("Preencha os campos!");
 		}
 		else
 		{
-			window.alert("Valor mínimo precisa ser menor que valor máximo");
+			if(vmin < vmax)
+			{
+				var a = 0;
+				var list = [];
+				for(var i = vmin; i < vmax; i++)
+				{
+					if(i % 2 === 0 && i % 3 === 0 && i !== vmin && i !== vmax)
+					{
+						list.push(i);
+					}
+				}
+				a = list.length;
+			}
+			else
+			{
+				window.alert("Valor mínimo precisa ser menor que valor máximo");
+			}
 		}
+		paragraph.innerHTML = a;
+		console.log(paragraph.innerHTML);
 	}
-	resp1.innerHTML = a;
-	console.log(resp1.innerHTML);
